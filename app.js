24fab75731c1d1d1bcef89969c7882ff817e1556
thadubsdavid <david76829@gmail>
2019-05-20 19:39:07 +0200

    Lines and Network button

--- app.js
+++ app.js
@@ -191,8 +191,34 @@ window.onclick = function(event) {
   }
 }
 
+//Sidebar LINES-------------------------------------------------------------------------------------------------------------------------------------
+// toggle functiono hides/shows LINES Sidebar
 
-//Slider------------------------------------------------------------------------
+function x() {
+
+  var sidebarLin = document.getElementById("sidebarLine")
+
+  if(sidebarLin.style.display === "inline-block") {
+    sidebarLin.style.display = "none";
+  } else {
+    sidebarLin.style.display = "inline-block";
+  }
+}
+
+//Sidebar NETWORK------------------------------------------------------------------------------------------------------------------------------------
+
+function y() {
+
+  var sidebarSub = document.getElementById("sidebarSuburb")
+
+  if(sidebarSub.style.display === "inline-block") {
+    sidebarSub.style.display = "none";
+  } else {
+    sidebarSub.style.display = "inline-block";
+  }
+}
+
+//Slider----------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
 var dataTime = d3.range(0,10).map(function(d) {
