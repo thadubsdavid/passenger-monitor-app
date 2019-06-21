7fd9e80aecd3268e3938b09f8019deab282363a7
thadubsdavid <david76829@gmail>
2019-06-21 08:48:02 +0200

    feature Serv

---
+++ public/javascripts/networkStops.js
@@ -0,0 +1,248 @@
+/*
+var geojson = {
+  
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+       "negativ": "null",
+       "positiv": 10,
+        "title": "Europaplatz",
+        "condition": "Accessible",
+        "lines": "S1, S2, 1, 2, 3, 6",
+        "entry": "700",
+        "exit": "500",
+        "passengerTotal": "980",
+        "passengerPreSession": 3,
+        "plan": "https://projekte.kvv-efa.de/sl3-alone/XSLT_DM_REQUEST?language=de&std3_suggestMacro=std3_suggest&std3_commonMacro=dm&std3_contractorMacro=&includeCompleteStopSeq=1&mergeDep=1&mode=direct&useRealtime=1&nameInfo_dm=7000031&type_dm=any&sessionID=0&requestID=0&itdLPxx_directRequest=1&coordOutputFormat=WGS84[dd.ddddd]"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.394767045974731,
+          49.00991641074167
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+       // "negativ": "null",
+        //"positiv": 0,
+        "title": "Herrenstrasse",
+        "condition": "Accessible",
+        "lines": "S1, S2, T1 ",
+        "entry": "400",
+        "exit": "300",
+        "passengerTotal": "350",
+        "passengerPreSession": 15,
+        "plan": "https://projekte.kvv-efa.de/sl3-alone/XSLT_DM_REQUEST?language=de&std3_suggestMacro=std3_suggest&std3_commonMacro=dm&std3_contractorMacro=&includeCompleteStopSeq=1&mergeDep=1&mode=direct&useRealtime=1&nameInfo_dm=7000099&type_dm=any&sessionID=0&requestID=0&itdLPxx_directRequest=1&coordOutputFormat=WGS84[dd.ddddd]"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.400356769561768,
+          49.00974751409709
+        ]
+      }
+    },
+  {
+    "type": "Feature",
+    "properties": {
+      "marker-color": "#7e7e7e",
+      //"positiv": 3,
+      //"negativ": ,
+      "title": "Marktplatz",
+      "condition": "None",
+      "lines": "S1, S2, T1",
+      "entry": "200",
+      "exit": "150",
+      "passengerTotal": "175",
+      "passengerPreSession": -13,
+      "plan": "https://projekte.kvv-efa.de/sl3-alone/XSLT_DM_REQUEST?language=de&std3_suggestMacro=std3_suggest&std3_commonMacro=dm&std3_contractorMacro=&includeCompleteStopSeq=1&mergeDep=1&mode=direct&useRealtime=1&nameInfo_dm=7000001&type_dm=any&sessionID=0&requestID=0&itdLPxx_directRequest=1&coordOutputFormat=WGS84[dd.ddddd]"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        8.404841423034668,
+        49.0095293550832
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "marker-color": "#7e7e7e",
+      //"positiv": 7,
+      "title": "Kronenplatz",
+      "condition": "Accessible",
+      "lines": "S1, S2, T1",
+      "entry": 450,
+      "exit": 350,
+      "passengerTotal": 400,
+      "passengerPreSession": 3,
+      "plan": "https://projekte.kvv-efa.de/sl3-alone/XSLT_DM_REQUEST?language=de&std3_suggestMacro=std3_suggest&std3_commonMacro=dm&std3_contractorMacro=&includeCompleteStopSeq=1&mergeDep=1&mode=direct&useRealtime=1&nameInfo_dm=7000002&type_dm=any&sessionID=0&requestID=0&itdLPxx_directRequest=1&coordOutputFormat=WGS84[dd.ddddd]"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        8.409991264343262,
+        49.009247858168976
+      ]
+    }
+  },
+  {
+    "type": "Feature",
+    "properties": {
+      "marker-color": "#7e7e7e",
+      //"positiv": 7,
+      
+
+      "title": "Durlacher Tor",
+      "condition": "Limited",
+      "lines": "S1, S2, T1",
+      "entry": 200,
+      "exit": 150,
+      "passengerTotal": 175,
+      "passengerPreSession": -13,
+      "plan": "https://projekte.kvv-efa.de/sl3-alone/XSLT_DM_REQUEST?language=de&std3_suggestMacro=std3_suggest&std3_commonMacro=dm&std3_contractorMacro=&includeCompleteStopSeq=1&mergeDep=1&mode=direct&useRealtime=1&nameInfo_dm=7000001&type_dm=any&sessionID=0&requestID=0&itdLPxx_directRequest=1&coordOutputFormat=WGS84[dd.ddddd]"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        8.4182,
+        49.0091
+      ]
+    }
+  },
+]
+};
+
+
+
+
+//var geojson = JSON.parse(data);
+//var arr = $.map(geojson.features.properties.passengerPreSession, function(el) { return el });
+//console.log(arr)
+//var arr = Object.keys(geojson).map(function(k) { return obj[k] });
+/*
+for (var i = 0; i < geojson.features.length; i++) {
+  var read = geojson.features[i].properties.passengerPreSession;
+  console.log(read)
+}
+
+for (var i = 0; i < geojson.features.length; i++) {
+  var read2 = geojson.features[i].properties.passengerPreSession;
+  console.log(read2)
+}
+
+for (read > 0) {
+
+  console.log('read bigger 0')
+  geojson.features[i].properties.passengerPreSession(function(marker){
+    var pos = document.createElement('div');
+    pos.className = 'marker2';
+  
+      isPositive = false;
+    new mapboxgl.Marker(pos)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25})
+    .setHTML('<h4>' + marker.properties.title + '</h4><p1>' + "Accessibility:\n" + marker.properties.condition + '\n' + '</p1><br><p2>' + "\nLines:\n" + marker.properties.lines + '</p2><br><p3>' + "Entry\n(P/h):\n" + marker.properties.entry + '</p3><br><p4>'+ "Exit\n(P/h):\n" + marker.properties.exit + '</p4><br><p5>'+ "Passenger\nFlow\n(P/h):\n" + marker.properties.passengerTotal + '</p5><br><p6>' + "Pre-Hour\n(%):\n" + marker.properties.passengerPreSession + '\n' + '<img src="images/rotesDreieck.jpg" alt="rotesDreieck" width="14px" height= "10px"></p6><br><p7>' + "Timetable:\n" + '</p7>' + '<a  href="' + marker.properties.plan +'">LIVE!</a><br><p8>' + "\nP/h = Passenger per hour\n" + '</p8>'))
+    .addTo(map)
+  });
+  
+//red
+}
+
+if (read2 = 0) {
+
+  geojson.features.forEach(function(marker){
+             
+    var el = document.createElement('div');
+    el.className = 'marker';
+      
+    isPositive = true;
+    
+    new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25})
+    .setHTML('<h4>' + marker.properties.title + '</h4><p1>' + "Accessibility:\n" + marker.properties.condition + '\n' + '</p1><br><p2>' + "\nLines:\n" + marker.properties.lines + '</p2><br><p3>' + "Entry\n(P/h):\n" + marker.properties.entry + '</p3><br><p4>'+ "Exit\n(P/h):\n" + marker.properties.exit + '</p4><br><p5>'+ "Passenger\nFlow\n(P/h):\n" + marker.properties.passengerTotal + '</p5><br><p6>' + "Pre-Hour\n(%):\n" + marker.properties.passengerPreSession + '\n' + '<img src="images/greenTriangle.png" alt="greenTriangle" width="14px" height= "10px"></p6><br><p7>' + "Timetable:\n" + '</p7>' + '<a  href="' + marker.properties.plan +'">LIVE!</a><br><p8>' + "\nP/h = Passenger per hour\n" + '</p8>'))
+    .addTo(map)
+    });
+    //green
+
+}
+*/
+  /*
+  //var description = our_data.properties[''].description.join('\n');
+  for (var i = 0; i < geojson.features.length; i++) {
+    //var itemTitle = geojson.features[i].title;
+   // var itemUrl = geojson.features[i].condition;
+    var itemPositv = geojson.features[i].properties.positiv;
+   // var itemNegativ = geojson.features[i].properties.negativ;
+    //var itemPassengerPreSession = geojson.features[i].properties.passengerPreSession;
+
+          console.log('positv: ' + itemPositv);
+
+          var isPositive = false;
+                      
+          if (parseInt(itemPositv) > 5 ) {
+            console.log('if1')
+              
+              geojson.features.forEach(function(marker){
+             
+              var el = document.createElement('div');
+              el.className = 'marker';
+                
+              isPositive = true;
+              
+              new mapboxgl.Marker(el)
+              .setLngLat(marker.geometry.coordinates)
+              .setPopup(new mapboxgl.Popup({ offset: 25})
+              .setHTML('<h4>' + marker.properties.title + '</h4><p1>' + "Accessibility:\n" + marker.properties.condition + '\n' + '</p1><br><p2>' + "\nLines:\n" + marker.properties.lines + '</p2><br><p3>' + "Entry\n(P/h):\n" + marker.properties.entry + '</p3><br><p4>'+ "Exit\n(P/h):\n" + marker.properties.exit + '</p4><br><p5>'+ "Passenger\nFlow\n(P/h):\n" + marker.properties.passengerTotal + '</p5><br><p6>' + "Pre-Hour\n(%):\n" + marker.properties.passengerPreSession + '\n' + '<img src="images/greenTriangle.png" alt="greenTriangle" width="14px" height= "10px"></p6><br><p7>' + "Timetable:\n" + '</p7>' + '<a  href="' + marker.properties.plan +'">LIVE!</a><br><p8>' + "\nP/h = Passenger per hour\n" + '</p8>'))
+              .addTo(map)
+              });
+              //green
+            
+            
+            }
+
+          else  {
+
+
+              console.log('else')
+              geojson.features.forEach(function(marker){
+                var pos = document.createElement('div');
+                pos.className = 'marker2';
+              
+                  isPositive = false;
+                new mapboxgl.Marker(pos)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 25})
+                .setHTML('<h4>' + marker.properties.title + '</h4><p1>' + "Accessibility:\n" + marker.properties.condition + '\n' + '</p1><br><p2>' + "\nLines:\n" + marker.properties.lines + '</p2><br><p3>' + "Entry\n(P/h):\n" + marker.properties.entry + '</p3><br><p4>'+ "Exit\n(P/h):\n" + marker.properties.exit + '</p4><br><p5>'+ "Passenger\nFlow\n(P/h):\n" + marker.properties.passengerTotal + '</p5><br><p6>' + "Pre-Hour\n(%):\n" + marker.properties.passengerPreSession + '\n' + '<img src="images/rotesDreieck.jpg" alt="rotesDreieck" width="14px" height= "10px"></p6><br><p7>' + "Timetable:\n" + '</p7>' + '<a  href="' + marker.properties.plan +'">LIVE!</a><br><p8>' + "\nP/h = Passenger per hour\n" + '</p8>'))
+                .addTo(map)
+              });
+              
+            }
+          }
+          //red
+              
+            
+          
+
+          if(isPositive == true)
+          {
+              //zeichen setzen auf gruen 
+          }
+          else{
+
+          }
+    
+          */
+  
+  
+    
+    
---
+++ public/javascripts/statisticsLive.js
@@ -0,0 +1,200 @@
+
+//LIVE MODAL Bootstrap----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Math.floor(Math.random() * (max - min + 1)) + min;
+var startModal = Array.from({ length: 10 }, () => Math.floor(Math.random() * 11)+10);
+var startModalShort = Array.from({ length: 5 }, () => Math.floor(Math.random() * 51)+20);
+var highModal = Array.from({ length: 10 }, () => Math.floor(Math.random() * 51) + 100);
+var startModalRev = startModal.reverse();
+
+var modalTrend = startModal.concat(highModal);
+var modalTrend2 = startModalShort.concat(highModal);
+var modalTrend3 = startModalShort.concat(startModal,startModalShort);
+var modalTrend4 = startModal.concat(startModalRev);
+var analysis = startModalShort.concat(highModal,startModal);
+
+var lineChart11 = document.getElementById("lineChart11");
+
+if (lineChart11) { 
+    
+    var chart = new Chart(lineChart11, {
+        // The type of chart we want to create
+        id: 'chartLine11',
+        type: 'line',
+        // The data for our dataset
+        data: {
+            labels: [
+                'Badenialp.',	'Wilhelm-Leu.-Str',	'Oberreut Zentrum',	'Albert-Braun-Straße',	'Hardecksiedlung', 'Bannwaldallee',	'Europahalle',	'Landesbausparkasse',	'Weinbrennerplatz',	'Sophienstraße', 'Schillerstraße',	'Mühlburger Tor',	'Europaplatz',	'Herrenstraße',	'Marktplatz',	'Kronenplatz',	'Durlacher Tor / K  I  T', 'Gottesauer Platz / B G V',	'Tullastraße / V B K',	'Weinweg',	'Untermühlstraße',	'Auer Straße/Dr.W Schwabe', 'Friedrichschule',	'Turmberg',
+            ],
+            datasets: [
+                
+                {
+                label: 'History of occupancy level: Oberreut - Durlach',
+                backgroundColor: [
+                    
+                    '#BCE6EE' //green
+                ],
+                borderColor: 'rgb(47,79,79)', //darkslategrey
+                hoverBackgroundColor: 'red',
+                data: modalTrend
+            },{
+                label: 'Expected by Analysis: Oberreut - Durlach',
+               
+                borderColor: 'rgb(178,34,34)', //red
+                hoverBackgroundColor: '#00394A',
+                data: analysis
+            }
+        
+        ]
+        },
+    
+        // Configuration options go here
+        options:{
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 0,
+                        suggestedMax: 200
+                    }
+                }]
+            }
+        }
+});
+}
+
+var lineChart12 = document.getElementById("lineChart12");
+
+
+
+if (lineChart12) { 
+    
+    var chart = new Chart(lineChart12, {
+        // The type of chart we want to create
+        id: 'chartLine12',
+        type: 'line',
+        // The data for our dataset
+        data: {
+            labels: [
+                "Turmberg", "Friedrichschule", "Auer Straße/Dr.W Schwabe", "Untermühlstraße", "Weinweg", "Tullastraße / V B K", "Gottesauer Platz / B G V", "Durlacher Tor / K  I  T", "Kronenplatz", "Marktplatz","Herrenstraße","Europaplatz", "Mühlburger Tor", "Schillerstraße","Sophienstraße","Weinbrennerplatz","Landesbausparkasse",
+                "Europahalle",
+                "Bannwaldallee",
+                "Hardecksiedlung",
+                "Albert-Braun-Straße",
+                "Oberreut Zentrum",
+                "Wilhelm-Leuschner-Straße",
+                "Badenialp."
+            ],
+            datasets: [{
+                label: 'History of occupancy level: Durlach - Oberreut',
+                backgroundColor: [
+                    '#BCE6EE' //green
+                ],
+                borderColor: 'rgb(47,79,79)', //darkslatelgrey
+                hoverBackgroundColor: 'red',
+                data: modalTrend2
+            },{
+                label: 'Expected by Analysis: Oberreut - Durlach',
+               
+                borderColor: 'rgb(178,34,34)', //red
+                hoverBackgroundColor: '#00394A',
+                data: analysis
+            }
+        ]
+        },
+    
+        // Configuration options go here
+        options:{
+            
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        suggestedMin: 0,
+                        suggestedMax: 200
+                    }
+                }]
+            }
+        
+        }
+    
+    });
+}
+
+var lineChart21 = document.getElementById("lineChart21");
+
+if (lineChart21) { 
+    
+    var chart = new Chart(lineChart21, {
+        // The type of chart we want to create
+        id: 'chartLine21',
+        type: 'line',
+        // The data for our dataset Siemensalle Wolfartsweier
+        data: {
+            labels: [
+                'Lassallestr',	'Siemensallee',	'Neureuter Str',	'Feierabendweg',	'Hertzstr', 'Stadt. Klinikum',	'Yorckstr',	'Mühlburger Tor',	'Europaplatz',	'Karlstor', 'Mathystr',	'Otto-Sachs-Str',	'Arbeitsagentur',	'ZKM',	'Welfenstr',	'Barbarossaplatz',	'Ebertstr', 'Hbf Vorplatz',	'Poststr',	'Tivoli',	'Werderstr',	'Rüppurer Tor', 'Kronenplatz',	'Durlacher Tor', 'Gottesauer Platz',	'Tulla Str',	'Weinweg', 'Auer Straße', 'Gritznerstr',	'Killisfelderweg', ' Ellmendinger Str', 'Ostmark Str',	'Schlesier Str',	'Steiermärker Str', 'Zündhütle', 'Wolfartsweier',
+
+            ],
+            datasets: [{
+                label: 'History of occupancy level: Siemensallee - Wolfartsweier',
+                backgroundColor: [
+                    
+                    '#BCE6EE'
+                ],
+                borderColor: 'rgb(0,26,4)',
+                hoverBackgroundColor: '#00394A',
+                data: modalTrend3
+            }]
+        },
+    
+        // Configuration options go here
+        options:{
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 0,
+                        suggestedMax: 200
+                    }
+                }]
+            }
+        }
+});
+}
+
+var lineChart22 = document.getElementById("lineChart22");
+
+if (lineChart22) { 
+    
+    var chart = new Chart(lineChart22, {
+        // The type of chart we want to create
+        id: 'chartLine22',
+        type: 'line',
+        // The data for our dataset
+        data: {
+            labels: [
+
+               "Wolfartsweier",'Zündhütle','SteiermärkerStr' ,'SchlesierStr' ,'OstmarkStr' , 'EllmendingerStr' ,'Killisfelderweg','Gritznerstr','Auer Straße','Weinweg','Tulla Str','Gottesauer Platz','Durlacher Tor','Kronenplatz','Rüppurer Tor','Werderstr','Tivoli','Poststr','Hbf Vorplatz','Ebertstr','Barbarossaplatz','Welfenstr','ZKM','Arbeitsagentur','Otto-Sachs-Str','Mathystr','Karlstor','Europaplatz','Mühlburger Tor','Yorckstr','Stadt. Klinikum','Hertzstr','Feierabendweg','Neureuter Str','Siemensallee',"Lassallestr"
+            ],
+            datasets: [{
+                label: 'History of occupancy level: Wolfartsweier - Siemensallee',
+                backgroundColor: [
+                    
+                    '#BCE6EE'
+                ],
+                borderColor: 'rgb(0,26,4)',
+                hoverBackgroundColor: '#00394A',
+                data:  modalTrend4
+            }]
+        },
+    
+        // Configuration options go here
+        options:{
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 0,
+                        suggestedMax: 200
+                    }
+                }]
+            }
+        }
+});
+}
